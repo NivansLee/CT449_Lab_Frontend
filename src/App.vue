bcb22a81ba8579f829a208bce0faddf90cc6224a
NivansLee <khoaledang2003.stvn@gmail.com>
2024-10-31 10:51:56 +0700

    Tao trang cap nhat lien he

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <script>
 import AppHeader from "@/components/AppHeader.vue";
 export default {
-components: {
-AppHeader,
-},
+  components: {
+    AppHeader,
+  },
 };
 </script>
 <template>
-<AppHeader />
-<div class="container mt-3">
-<router-view />
-</div>
+  <AppHeader />
+  <div class="container mt-3">
+    <router-view />
+  </div>
 </template>
 <style>
 .page {
-max-width: 400px;
-margin: auto;
+  max-width: 400px;
+  margin: auto;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+import ContactForm from "@/components/ContactForm.vue";
 const routes = [
     {
         path: "/",
         name: "contactbook",
         component: ContactBook,
+        children: []
     },
 
     {
         path: "/:pathMatch(.*)*",
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
+    },
+
+    {
+        path: "/contact/add",
+        name: "contact.add",
+        component: ContactForm,
+    },
+
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
     },
 
 ];
