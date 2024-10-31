0f2b386a8d57fe302fef030e565e63c8b6bee69d
NivansLee <khoaledang2003.stvn@gmail.com>
2024-10-31 08:28:05 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,12 @@ const routes = [
         component: ContactBook,
     },
 
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
