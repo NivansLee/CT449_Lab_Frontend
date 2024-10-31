ad6e44ae4e927a78dbe1e46d10943006e5241620
NivansLee <khoaledang2003.stvn@gmail.com>
2024-10-31 11:54:30 +0700

    Tao trang them lien he moi

--- src/router/index.js
+++ src/router/index.js
@@ -16,16 +16,16 @@ const routes = [
     },
 
     {
-        path: "/contact/add",
-        name: "contact.add",
-        component: ContactForm,
-    },
-
-    {
         path: "/contacts/:id",
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
+    },
+
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
     },
 
 ];
